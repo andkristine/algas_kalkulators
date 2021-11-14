97134721eb4c76100a925768b717ee469dde6ebc
andkristine <57807427+andkristine@users.noreply.github.com>
2021-11-14 21:34:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $vaiGramatina = "Vai Jums ir iesniegta nodokļu grāmatiņa?"
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style/style.css"> 
+    <link rel="stylesheet" href="./style/style.css"> 
     <title>Algas kalkulators</title>
     </head>
         <body>
@@ -42,5 +42,5 @@ $vaiGramatina = "Vai Jums ir iesniegta nodokļu grāmatiņa?"
             </div>
         </div>    
     </body>
-    <script src="/js/app.js"></script>
+    <script src="./js/app.js"></script>
 </html>
